df4648c9abb964ee5de91bb608d37fbfabaec592
Paulo Costa <me@paulo.costa.nom.br>
2017-08-18 13:17:20 -0300

    Remove extra nested funcion

--- draggable-3d.js
+++ draggable-3d.js
@@ -14,108 +14,106 @@
 	} else {
 		factory(Highcharts);
 	}
-}(function (Highcharts) {
+}(function (H) {
 	"use strict";
 
-	(function (H) {
-		H.getOptions().chart.options3d.drag = {
-			enabled: false,
-			minAlpha: -90,
-			maxAlpha:  90,
-			minBeta:  -90,
-			maxBeta:   90,
-			snap: 0,
-			animateSnap: false,
-			speed: null,     // Dragging speed, in º/pixel. A good speed will be calculate on the fly if not specified
-			flipAxes: false  // Automatically change axis positions according to the angles
-		};
+	H.getOptions().chart.options3d.drag = {
+		enabled: false,
+		minAlpha: -90,
+		maxAlpha:  90,
+		minBeta:  -90,
+		maxBeta:   90,
+		snap: 0,
+		animateSnap: false,
+		speed: null,     // Dragging speed, in º/pixel. A good speed will be calculate on the fly if not specified
+		flipAxes: false  // Automatically change axis positions according to the angles
+	};
 
-		H.wrap(H.Chart.prototype, "init", function (proceed) {
-			proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+	H.wrap(H.Chart.prototype, "init", function (proceed) {
+		proceed.apply(this, Array.prototype.slice.call(arguments, 1));
 
-			var chart = this;
-			if (!chart.is3d || !chart.is3d()) {
-			return;
+		var chart = this;
+		if (!chart.is3d || !chart.is3d()) {
+		return;
+		}
+
+		// Add mouse events for rotation
+		var mouseDown = function (e) {
+			var options3d = chart.options.chart.options3d,
+				dragOptions = options3d.drag,
+				eStart = chart.pointer.normalize(e),
+				startAlpha = options3d.alpha,
+				startBeta = options3d.beta,
+				speed = H.pick(  // degree/pixel - higher is more sensitive
+					dragOptions.speed,
+					H.pick(dragOptions.speedScale, 1) * 360 / (Math.PI * Math.max(chart.plotWidth, chart.plotHeight, options3d.depth)));  // Calculate a "natural" speed, proportional to the chart's biggest dimension
+
+			if (dragOptions.enabled) {
+				var setOrientation = function(newAlpha, newBeta, animate) {
+					newAlpha = Math.min(dragOptions.maxAlpha, Math.max(dragOptions.minAlpha, newAlpha));
+					newBeta  = Math.min(dragOptions.maxBeta,  Math.max(dragOptions.minBeta,  newBeta ));
+					options3d.alpha = newAlpha;
+					options3d.beta  = newBeta;
+
+					if (dragOptions.flipAxes) {
+						H.each(chart.xAxis, function(axis) {
+							var opposite = newAlpha < 0;
+							if (opposite !== axis.opposite) {
+								axis.update({
+									opposite: opposite
+								}, animate);
+							}
+						});
+						H.each(chart.yAxis, function(axis) {
+							var opposite = newBeta < 0;
+							if (opposite !== axis.opposite) {
+								axis.update({
+									opposite: opposite
+								}, animate);
+							}
+						});
+						H.each(chart.zAxis, function(axis) {
+							var opposite = newAlpha < 0;
+							if (opposite !== axis.opposite) {
+								axis.update({
+									opposite: opposite
+								}, animate);
+							}
+						});
+					}
+					chart.redraw(animate);
+				};
+
+				var mouseMoved, mouseReleased;
+
+				mouseMoved = function (e) {
+					//Calculate new angle
+					e = chart.pointer.normalize(e);
+					var newAlpha = startAlpha + (e.chartY - eStart.chartY) * speed;
+					var newBeta = startBeta + (eStart.chartX - e.chartX) * speed;
+					setOrientation(newAlpha, newBeta, false);
+				};
+
+				mouseReleased = function () {
+					H.removeEvent(document, "mousemove", mouseMoved);
+					H.removeEvent(document, "touchdrag", mouseMoved);
+					H.removeEvent(document, "mouseup",   mouseReleased);
+					H.removeEvent(document, "touchend",  mouseReleased);
+
+					if (dragOptions.snap) {
+						var snapAlpha = Math.round(options3d.alpha / dragOptions.snap) * dragOptions.snap;
+						var snapBeta  = Math.round(options3d.beta / dragOptions.snap) * dragOptions.snap;
+						setOrientation(snapAlpha, snapBeta, dragOptions.animateSnap);
+					}
+				};
+
+				H.addEvent(document, "mousemove", mouseMoved);
+				H.addEvent(document, "touchmove", mouseMoved);
+				H.addEvent(document, "mouseup",   mouseReleased);
+				H.addEvent(document, "touchend",  mouseReleased);
 			}
-
-			// Add mouse events for rotation
-			var mouseDown = function (e) {
-				var options3d = chart.options.chart.options3d,
-					dragOptions = options3d.drag,
-					eStart = chart.pointer.normalize(e),
-					startAlpha = options3d.alpha,
-					startBeta = options3d.beta,
-					speed = H.pick(  // degree/pixel - higher is more sensitive
-						dragOptions.speed,
-						H.pick(dragOptions.speedScale, 1) * 360 / (Math.PI * Math.max(chart.plotWidth, chart.plotHeight, options3d.depth)));  // Calculate a "natural" speed, proportional to the chart's biggest dimension
-
-				if (dragOptions.enabled) {
-					var setOrientation = function(newAlpha, newBeta, animate) {
-						newAlpha = Math.min(dragOptions.maxAlpha, Math.max(dragOptions.minAlpha, newAlpha));
-						newBeta  = Math.min(dragOptions.maxBeta,  Math.max(dragOptions.minBeta,  newBeta ));
-						options3d.alpha = newAlpha;
-						options3d.beta  = newBeta;
-
-						if (dragOptions.flipAxes) {
-							H.each(chart.xAxis, function(axis) {
-								var opposite = newAlpha < 0;
-								if (opposite !== axis.opposite) {
-									axis.update({
-										opposite: opposite
-									}, animate);
-								}
-							});
-							H.each(chart.yAxis, function(axis) {
-								var opposite = newBeta < 0;
-								if (opposite !== axis.opposite) {
-									axis.update({
-										opposite: opposite
-									}, animate);
-								}
-							});
-							H.each(chart.zAxis, function(axis) {
-								var opposite = newAlpha < 0;
-								if (opposite !== axis.opposite) {
-									axis.update({
-										opposite: opposite
-									}, animate);
-								}
-							});
-						}
-						chart.redraw(animate);
-					};
-
-					var mouseMoved, mouseReleased;
-
-					mouseMoved = function (e) {
-						//Calculate new angle
-						e = chart.pointer.normalize(e);
-						var newAlpha = startAlpha + (e.chartY - eStart.chartY) * speed;
-						var newBeta = startBeta + (eStart.chartX - e.chartX) * speed;
-						setOrientation(newAlpha, newBeta, false);
-					};
-
-					mouseReleased = function () {
-						H.removeEvent(document, "mousemove", mouseMoved);
-						H.removeEvent(document, "touchdrag", mouseMoved);
-						H.removeEvent(document, "mouseup",   mouseReleased);
-						H.removeEvent(document, "touchend",  mouseReleased);
-
-						if (dragOptions.snap) {
-							var snapAlpha = Math.round(options3d.alpha / dragOptions.snap) * dragOptions.snap;
-							var snapBeta  = Math.round(options3d.beta / dragOptions.snap) * dragOptions.snap;
-							setOrientation(snapAlpha, snapBeta, dragOptions.animateSnap);
-						}
-					};
-
-					H.addEvent(document, "mousemove", mouseMoved);
-					H.addEvent(document, "touchmove", mouseMoved);
-					H.addEvent(document, "mouseup",   mouseReleased);
-					H.addEvent(document, "touchend",  mouseReleased);
-				}
-			};
-			H.addEvent(chart.container, "mousedown", mouseDown);
-			H.addEvent(chart.container, "touchstart", mouseDown);
-		});
-	}(Highcharts));
+		};
+		H.addEvent(chart.container, "mousedown", mouseDown);
+		H.addEvent(chart.container, "touchstart", mouseDown);
+	});
 }));
